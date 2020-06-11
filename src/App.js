7efef1746190b1fd5a27bbcd484195098ef40299
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-06-11 15:21:29 -0400

    navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>getting started</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+const navbar =(props)=>(
+    
+    <ul className="Navbar">
+        <li className="item">Home</li>
+        <li className="item">Game</li>
+    </ul>
+    
+)
+export default navbar;
