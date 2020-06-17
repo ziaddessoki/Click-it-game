f9d2bd538bcfb89ac55cc3a885fdc9fb6c21cc0c
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-06-17 02:04:24 -0400

    functionality

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Actor(props) {
     return (
         <div>
-        <img src={props.src} width="250" height="150" onClick={props.imgClicked}/>
+        <img src={props.src} width="250" height="150" 
+        onClick={props.imgClicked}
+         alt={`actor${props.id}`}/>
         </div>
     )
 };
--- src/components/Game.js
+++ src/components/Game.js
@@ -9,29 +9,75 @@ class Game extends React.Component {
     score : 0,
     topScore: 0
   }
-  imageClicked =id =>{
-    let guessedRight = false;
+  shuffle=(data) =>{
+    let ctr = data.length, temp, index;
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = data[ctr];
+        data[ctr] = data[index];
+        data[index] = temp;
+    }
+    return data;
+  }
+  correctAnswer = data =>{
+    const{ score,topScore } = this.state
+    const newScore = score + 10
+    const NewTopScore = Math.max(newScore,topScore)
+    console.log(newScore)
+
+    this.setState({
+      data:this.shuffle(data),
+      score: newScore,
+      topScore: NewTopScore
+    })
+    
+  }
+
+  wrongAnswer = data =>{
+    
+    this.setState({
+      score:0,
+      data: this.resetData(data)
+    })
+  }
+
+  resetData = data => {
+    const resetData = data.map(item => ({ ...item, clicked: false }));
+    return this.shuffle(resetData);
+  };
+
+
+   imageClicked = (id) =>{
+    let rightGuess = false;
+    console.log(id)
     const newData = this.state.data.map( person =>{
-      let newPerson ={...person};
-      if(newPerson.id == id){
+    const newPerson ={...person};
+      if(newPerson.id === id){
+        console.log(newPerson.id)
         if (!newPerson.clicked){
-          guessedRight =true;
-          newPerson.clicked=true;
+          rightGuess = true;
+          newPerson.clicked= true;
         }
       }
-      return newPerson
+      return newPerson;
+      
     })
-    guessedRight? correctAnswer(newData) : wrongAnswer(newData)
+    rightGuess ? this.correctAnswer(newData) : this.wrongAnswer(newData);
   }
 
     render() {
       return (
           <div className="Game">
+            <h1 style={{color:'white'}}>{this.state.score}</h1>
+            <h1 style={{color:'white'}}>{this.state.topScore}</h1>
               {this.state.data.map(person =>(
-                <div>
                 <Actor key={person.id} src={person.image}
-                imgClicked={this.imageClicked}></Actor>
-                </div>
+                imgClicked={() =>this.imageClicked(person.id)}></Actor>
               ))}
           </div>
       )
