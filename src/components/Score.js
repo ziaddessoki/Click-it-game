751e74586cbbdff509650f080eaf521eddf60427
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-06-19 22:45:55 -0400

    score and styling

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Score.css'
 
 function Score(props) {
     return (
-        <div>
+        <div className={Score}>
             <h1>Score:{props.score} ||  Top Score:{props.topScore}</h1>
         </div>
     )
